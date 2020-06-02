36893d0e0fae044bc7489054339629a0516674e3
Sajeeb <sajeeb@Sajeebs-MacBook-Pro.local>
2020-06-02 04:52:32 -0600

    -> updated makefile in examples -> updated readme file with verbosity example

--- examples/verbosity.cpp
+++ examples/verbosity.cpp
@@ -6,6 +6,11 @@
  */
 
 #include <CPPLOGGER.h>
+
+/**
+ * The following should always be called only in the main translation unit.
+ * This is to default initialize the logger flags.
+ */
 logger_init();
 
 int main() {
